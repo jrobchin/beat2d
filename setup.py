21cf92997a726df594226cf29485a31772c7bcca
Jason Chin <jason.rob.chin@gmail.com>
2021-02-28 10:18:01 -0500

    make library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="beat2d",
+    version="0.1",
+    packages=[
+        "beat2d",
+    ],
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
+    long_description=open("README.md").read(),
+)
